90861b9ebd5ba03dcceb92e19f8dcdce3b859da8
CrimsonDemon567PC <xxw4pts572@privaterelay.appleid.com>
2026-01-30 11:30:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     python_requires=">=3.8",
     license="MIT",
     description="LENS v3.1 – Hardened reference implementation for a custom binary data format",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
     author="Dein Name",
     url="https://github.com/CrimaomDemon567PC/LensFormat",
     classifiers=[
