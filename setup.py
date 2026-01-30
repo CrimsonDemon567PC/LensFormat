eda709668633c68c5b0383cddcdc3a8b700efb81
CrimsonDemon567PC <xxw4pts572@privaterelay.appleid.com>
2026-01-30 11:03:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lens_format",
+    version="3.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    description="LENS v3.1 – Hardened reference implementation for a custom binary data format",
+    author="Dein Name",
+    url="https://github.com/CrimaomDemon567PC/LensFormat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
